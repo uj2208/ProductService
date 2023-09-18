17ada7f2aa5f1bd3b84b6ca8be534a0540fa10e3
uj2208 <uj.verma42@icloud.com>
2023-09-18 14:02:11 +0530

    Cardinality mapping between Product and Category

--- src/main/java/dev/ujjwal/ProductService/models/Category.java
+++ src/main/java/dev/ujjwal/ProductService/models/Category.java
@@ -1,8 +1,22 @@
 package dev.ujjwal.ProductService.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
 
 @Entity
+@Getter
+@Setter
 public class Category  extends BaseModel{
+    @Column
     private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+    // this is the same relation being mapped by category attribute in the other (Product) class
+
 }
--- src/main/java/dev/ujjwal/ProductService/models/Product.java
+++ src/main/java/dev/ujjwal/ProductService/models/Product.java
@@ -2,11 +2,15 @@ package dev.ujjwal.ProductService.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product extends BaseModel{
     private String title;
     private String description;
